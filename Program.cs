e0b1437b5163612b97378fe59c1cfcacfbb88284
Rogério Alves dos Anjos <estatistica.rogerio.fontes@gmail.com>
2021-02-18 23:25:54 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+
+// ultima mensagem utilizada Msg 019
+
+namespace DIO.DosAnjos.Bank
+{
+	class Program
+	{
+		static List<Conta> listContas = new List<Conta>();
+		static void Main(string[] args)
+		{
+			string opcaoUsuario = ObterOpcaoUsuario();
+
+			while (opcaoUsuario.ToUpper() != "X")
+			{
+				switch (opcaoUsuario)
+				{
+					case "1":
+						InserirConta();
+						break;
+					case "2":						
+						ListarContas();
+						break;
+					case "3":
+						Depositar();
+						break;
+					case "4":
+						Sacar();
+						break;
+					case "5":
+						Transferir();
+						break;
+                    case "C":
+						Console.Clear();
+						break;
+					default:
+						throw new ArgumentOutOfRangeException();
+				}
+
+				opcaoUsuario = ObterOpcaoUsuario();
+			}
+			
+			Console.WriteLine("Msg 001 - Obrigado por utilizar nossos serviços.");
+			Console.ReadLine();
+		}
+
+		private static void InserirConta()
+		{
+			Console.WriteLine("  -----  Inserir nova conta  -----");
+
+			Console.Write("Msg 002 - Digite 1 para Conta Fisica ou 2 para Juridica: ");
+			int entradaTipoConta = int.Parse(Console.ReadLine());
+
+			Console.Write("Msg 003 - Digite o Nome do Cliente: ");
+			string entradaNome = Console.ReadLine();
+
+			Console.Write("Msg 004 - Digite o saldo inicial: ");
+			double entradaSaldo = double.Parse(Console.ReadLine());
+
+			Console.Write("Msg 005 - Digite o crédito: ");
+			double entradaCredito = double.Parse(Console.ReadLine());
+
+			Conta novaConta = new Conta(tipoConta: (TipoConta)entradaTipoConta,
+										saldo: entradaSaldo,
+										credito: entradaCredito,
+										nome: entradaNome);
+
+			listContas.Add(novaConta);
+		}
+
+		private static void ListarContas()
+		{
+			Console.WriteLine("  -----  Listar contas  -----  ");
+
+			if (listContas.Count == 0)
+			{
+				Console.WriteLine("Msg 006 - Nenhuma conta cadastrada.");
+				return;
+			}
+
+			for (int i = 0; i < listContas.Count; i++)
+			{
+				Conta conta = listContas[i];
+				Console.Write("#{0} - ", i);
+				Console.WriteLine(conta);
+			}
+		}
+
+		private static void Depositar()
+		{
+			Console.WriteLine("  -----  Depósito  -----");
+
+			Console.Write("Msg 007 - Digite o número da conta: ");
+			int indiceConta = int.Parse(Console.ReadLine());
+
+			Console.Write("Msg 008 - Digite o valor a ser depositado: ");
+			double valorDeposito = double.Parse(Console.ReadLine());
+
+            listContas[indiceConta].Depositar(valorDeposito);
+		}
+
+		private static void Sacar()
+		{
+			Console.WriteLine("  -----  Saque  -----");
+
+			Console.Write("Msg 009 - Digite o número da conta: ");
+			int indiceConta = int.Parse(Console.ReadLine());
+
+			Console.Write("Msg 010 - Digite o valor a ser sacado: ");
+			double valorSaque = double.Parse(Console.ReadLine());
+
+            listContas[indiceConta].Sacar(valorSaque);
+		}
+
+		private static void Transferir()
+		{
+			Console.WriteLine("  -----  Transferência entre contas  -----");
+
+			Console.Write("Msg 011 - Digite o número da conta de origem: ");
+			int indiceContaOrigem = int.Parse(Console.ReadLine());
+
+            Console.Write("Msg 012 - Digite o número da conta de destino: ");
+			int indiceContaDestino = int.Parse(Console.ReadLine());
+
+			Console.Write("Msg 013 - Digite o valor a ser transferido: ");
+			double valorTransferencia = double.Parse(Console.ReadLine());
+
+            listContas[indiceContaOrigem].Transferir(valorTransferencia, listContas[indiceContaDestino]);
+		}
+
+		private static string ObterOpcaoUsuario()
+		{
+			Console.WriteLine();
+			Console.WriteLine("---  DIO Dos-Anjos Bank a seu dispor!!! ---");
+			Console.WriteLine("Informe a opção desejada:");
+
+			Console.WriteLine("1 - Inserir nova conta");
+			Console.WriteLine("2 - Listar contas");
+			Console.WriteLine("3 - Depositar");
+			Console.WriteLine("4 - Sacar");
+			Console.WriteLine("5 - Transferir");
+            Console.WriteLine("C - Limpar Tela");
+			Console.WriteLine("X - Sair");
+			Console.WriteLine();
+
+			string opcaoUsuario = Console.ReadLine().ToUpper();
+			Console.WriteLine();
+			return opcaoUsuario;
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+# Digital Innovation One - Prática .NET
+
+## POO na prática: criando uma aplicação
+
+Construção da aplicação em .NET durante o laboratório do curso.
+Foram desenvolvidas as funções básicas para movimentação de uma conta bancária e utilizado como repositório
+uma List<T> interna a aplicação, aplicação essa desenvolvida em C# Core 5.0.
+
+Reestruturei o menu de opções (ações) e incluí códigos de mensagenspara uma melhor orientaçao em caso de
+necessidade de manutenção.
+
+## "DIO Dos-Anjos Bank a seu dispor!!!"
+## "Informe a opção desejada:"
+## "1 - Inserir nova conta"
+## "2 - Listar contas"
+## "3 - Depositar"
+## "4 - Sacar"
+## "5 - Transferir"
+## "C - Limpar Tela"
+## "X - Sair"
+
+#### -RAA-
+
+
